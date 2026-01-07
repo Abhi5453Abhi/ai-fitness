182f918bae48b8449cea70a459903cbc0ec91fab
Abhi5453Abhi <abhi5453abhi@gmail.com>
2026-01-07 05:50:48 +0530

    fix(uploadthing): increase video maxFileSize to 256MB

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -5,7 +5,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
-    videoUploader: f({ video: { maxFileSize: "64MB", maxFileCount: 1 } })
+    videoUploader: f({ video: { maxFileSize: "256MB", maxFileCount: 1 } })
         // Set permissions and file types for this FileRoute
         .middleware(async ({ req }) => {
             // This code runs on your server before upload
